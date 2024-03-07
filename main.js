654275b609db9338a3ce7fc3fb56a8fe97db16f5
Sharif-Mahmud <sharif230@gmail.com>
2024-03-08 04:49:46 +0600

    screen sharing section

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+   
+    $(".switch_btn").click(function () {
+        $(this).toggleClass("on");
+        $(".pricing_area .left").toggleClass("on");
+    });
+
+
+});
